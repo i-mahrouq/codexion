5dfd202b1f89d00cc255b785e6922b271a1e1920
i-mahrouq <ilyassmahrou9@gmail.com>
2026-06-13 13:07:28 +0100

    .

--- codexion.h
+++ codexion.h
@@ -1,7 +1,9 @@
 #ifndef CODEXION_H
 #define CODEXION_H
 
-
+#include <pthread.h>
+#include <sys/time.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -9,19 +11,30 @@ int is_valid(const char **input);
 
 
 typedef enum e_sched {
-    SCHED_FIFO,
-    SCHED_EDF
+    FIFO,
+    EDF
 }   t_sched;
 
 typedef struct s_config {
     int     num_coders;
-    long    time_to_burnout;
-    long    time_to_compile;
-    long    time_to_debug;
-    long    time_to_refactor;
+    int    time_to_burnout;
+    int    time_to_compile;
+    int    time_to_debug;
+    int    time_to_refactor;
     int     num_compiles_req;
-    long    dongle_cooldown;
+    int    dongle_cooldown;
     t_sched scheduler;
 }   t_config;
+
+typedef struct s_dongle {
+    int id;
+    int position;
+}
+
+typedef struct s_coder {
+    int id;
+    int count_comp;
+}
+
 
 #endif
